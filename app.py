022c4b6cda4b4ac041fb10d9cccb6a2f34bf6c24
jeison iglesias <jeisonmavisoy2020@itp.edu.co>
2021-09-03 21:24:18 -0500

    AGREGANDO INFORMCAION A LA BASES DE DATOS

--- app.py
+++ app.py
@@ -1,1 +1,30 @@
+import mysql.connector
 print('Iniciando')
+db = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='',
+    database='blog',
+    port=3306
+)
+def crearUsuario(nombre,email,contrasenna):
+    cursor=db.cursor()
+    cursor.execute('''
+        insert into usuarios(nombre,email,contrasena)
+        values (%s,%s,%s)''',
+        (nombre,
+        email,
+        contrasenna))
+
+    # Creacion, Modificacion, eliminacion de datos
+    db.commit()
+
+    #cerrar cursor
+    cursor.close()
+crearUsuario('fernando','yob@gmail.com','12212121')
+
+cursor =db.cursor()
+
+cursor.execute('select *from usuarios')
+usuarios = cursor.fetchall() 
+print(usuarios)
